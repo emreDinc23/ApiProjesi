f9a0797788a91e51eddb599f4f40fc7347ba76cd
Emre Dinç <105745981+emreDinc23@users.noreply.github.com>
2023-08-26 12:37:52 +0300

    com3

--- Frontend/HotelProject.WebUI/Startup.cs
+++ Frontend/HotelProject.WebUI/Startup.cs
@@ -7,6 +7,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using HotelProject.EntityLayer.Concreate;
 
 namespace HotelProject.WebUI
 {
@@ -22,13 +24,16 @@ namespace HotelProject.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(typeof(Startup));
             services.AddHttpClient();
             services.AddControllersWithViews();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
